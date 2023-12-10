fa27a703f2a6cad60042e312b4cf8310c94a2905
BlearyBonsai676 <rd.mrincredible@gmail.com>
2023-12-10 14:42:07 +0100

    minecraft erste version

---
+++ script.js
@@ -0,0 +1,18 @@
+// Number of Minecraft icons to display
+const numberOfIcons = 10000;
+
+// Function to create and append Minecraft icons
+function createIcons() {
+  const container = document.querySelector('.minecraft-icons');
+  
+  for (let i = 0; i < numberOfIcons; i++) {
+    const icon = document.createElement('div');
+    icon.classList.add('minecraft-icon');
+    icon.style.left = `${Math.random() * 99}%`; // Adjusted the range for positioning
+    icon.style.top = `${Math.random() * 99}%`; // Adjusted the range for positioning
+    container.appendChild(icon);
+  }
+}
+
+// Call the function to create icons when the page loads
+window.addEventListener('load', createIcons);
